59e9915febdb932bb9ec6c326c3f3f1e7a5fea88
sj <shijuuk2.98@gmail.com>
2026-03-12 11:07:18 +0530

    keil done build

--- src/scripts/test_repair_api_v2.ts
+++
@@ -1,8 +0,0 @@
-import fetch from 'node-fetch';
-
-async function test() {
-    const res = await fetch('http://localhost:5000/api/keil/fleet/repairs?company_id=5be36c2a-c42e-4469-8551-115ede9ca728');
-    const data = await res.json();
-    console.log(JSON.stringify(data, null, 2));
-}
-test();
